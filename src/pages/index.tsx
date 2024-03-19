8256064885cf0e3485ec25969ee7ac91d0f6a1e1
rmelinevskiy <150797126+Arkadaweb@users.noreply.github.com>
2024-03-19 20:52:34 +0300

    test fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,6 @@ const Home: FC<PropsWithChildren<any>> = ({
                                               meta
                                           }) => {
 
-    console.log(brands)
-
     return (
         <Meta title={'Главная'} metaData={meta}>
             <MainLayout
